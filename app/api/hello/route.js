4b7a9fd037c6a3ea2f8fa4db4075d05abd8360d6
Alex Raduca <alexandru.raduca@gmail.com>
2024-12-05 14:48:38 +0200

    template sacose v1

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,7 +1,10 @@
 // app/api/hello/route.js
-export async function GET(request) {
-  return new Response(JSON.stringify({ text: "Hello from Route Handler" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+export async function GET(request, { params }) {
+  return new Response(
+    JSON.stringify({ text: "Hello from Route Handler", params: params }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
--- app/api/pdf/route.js
+++ app/api/pdf/route.js
@@ -1,22 +1,26 @@
 // pages/api/generate-pdf.js
 import puppeteer from "puppeteer";
 
-import { NextResponse } from "next/server";
-
-export default async function POST(request) {
+export async function POST(request) {
   const data = await request.json();
   const { collection, keys } = data;
   console.log("request received: ", { collection, keys });
 
   // 1. Get HTML content from Next.js page
-  const url = `${process.env.APP_URL}/templates${collection}/${keys[0]}`;
+  const url = `${process.env.APP_URL}/templates/${collection}/${keys[0]}`;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  // const html = await page.content();
+
+  console.log("Page loaded: ", url);
 
   // 2. Convert to PDF
-  const pdfBuffer = await page.pdf({ format: "A4" });
+  const pdfBuffer = await page.pdf({
+    format: "A4",
+    printBackground: true,
+    displayHeaderFooter: false,
+    timeout: 0,
+  });
   await browser.close();
 
   console.log("PDF generated: ", pdfBuffer);
@@ -37,9 +41,8 @@ export default async function POST(request) {
 
   // 4. Return File ID and URL
 
-  return NextResponse.json({
-    message: "Data processed successfully",
-    fileId,
-    fileUrl,
+  return new Response(JSON.stringify({ fileData }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
   });
 }
